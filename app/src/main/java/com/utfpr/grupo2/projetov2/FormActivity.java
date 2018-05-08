77c67f2814422ec53050cb0a791c4dca54df8d83
Elio Costa <eliocostac4@gmail.com>
2018-05-08 19:56:07 -0300

    validações

--- app/src/main/java/com/utfpr/grupo2/projetov2/FormActivity.java
+++ app/src/main/java/com/utfpr/grupo2/projetov2/FormActivity.java
@@ -9,6 +9,7 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -19,23 +20,37 @@ public class FormActivity extends AppCompatActivity {
         setTitle("Cadastro");
     }
 
-    public void enviarForm(View v){
+    public void showMessage(String msg){
+        Toast.makeText(this,msg,Toast.LENGTH_LONG).show();
+    }
+
+    public boolean enviarForm(View v){
 
         EditText txtnomeDisciplina = (EditText)findViewById(R.id.nomeDisciplina);
         EditText txtaulasSemestre = (EditText)findViewById(R.id.aulasSemestre);
         EditText txtpercentFaltasPossiveis = (EditText)findViewById(R.id.percentFaltasPossiveis);
         EditText txtfaltasJaExistentes = (EditText)findViewById(R.id.faltasJaExistentes);
 
+        String nomeDisciplina = txtnomeDisciplina.getText().toString();
+        String aulasSemestre = txtaulasSemestre.getText().toString();
+        String percentFaltasPossiveis = txtpercentFaltasPossiveis.getText().toString();
+        String faltasJaExistentes = txtfaltasJaExistentes.getText().toString();
+
+        if(nomeDisciplina.equals("") || aulasSemestre.equals("") || percentFaltasPossiveis.equals("") || faltasJaExistentes.equals("")){
+            showMessage("Preecha todos os campos para continuar!");
+            return false;
+        }
+
         Intent intent = new Intent(this, ResultActivity.class);
         Bundle params = new Bundle();
 
-        params.putString("nome_disciplina", txtnomeDisciplina.getText().toString());
-        params.putString("aulas_semestre", txtaulasSemestre.getText().toString());
-        params.putString("percent_faltas", txtpercentFaltasPossiveis.getText().toString());
-        params.putString("faltas_existentes", txtfaltasJaExistentes.getText().toString());
-
+        params.putString("nome_disciplina", nomeDisciplina);
+        params.putString("aulas_semestre", aulasSemestre);
+        params.putString("percent_faltas", percentFaltasPossiveis);
+        params.putString("faltas_existentes", faltasJaExistentes);
 
         intent.putExtras(params);
         startActivity(intent);
+        return true;
     }
 }
